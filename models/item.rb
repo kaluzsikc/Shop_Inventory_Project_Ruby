5f956145c2a7dc575b886297fb7bfe255372f3c7
kaluzsikc <kaluzsikc@gmail.com>
2018-08-24 15:52:37 +0100

    Test item has id/ manuf_if/ name/ description/ cost & sell_price/ quantity/ min & max_stock

--- models/item.rb
+++ models/item.rb
@@ -2,14 +2,14 @@ require_relative("../db/sql_runner")
 
 class Item
 
-  attr_accessor :id, :name, :description, :cost_price, :sell_price, :quantity_in_stock, :manufacturer_id
+  attr_accessor :id, :name, :description, :cost_price, :sell_price, :quantity_in_stock, :manufacturer_id, :min_stock_level, :max_stock_level
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
     @name = options['name']
     @description = options['description']
-    @cost_price = options['cost_price'].to_i
-    @sell_price = options['sell_price'].to_i
+    @cost_price = options['cost_price'].to_f
+    @sell_price = options['sell_price'].to_f
     @quantity_in_stock = options['quantity_in_stock'].to_i
     @min_stock_level = options['min_stock_level'].to_i
     @max_stock_level = options['max_stock_level'].to_i
--- models/specs/items_spec.rb
+++ models/specs/items_spec.rb
@@ -5,14 +5,14 @@ class TestItem < MiniTest::Test
 
   def setup
     options = {"id" => 1,
-              "manufacturer_id" => 1,
+              "manufacturer_id" => 20,
               "name" => "TV",
               "description" => "Smart TV",
-              "cost_price" => 100.00,
+              "cost_price" => 100.11,
               "sell_price" => 200,
               "quantity_in_stock" => 10,
-              "min_stock_level" =>2,
-              "max_stock_level" =>50}
+              "min_stock_level" => 2,
+              "max_stock_level" => 50 }
 
     @item = Item.new(options)
   end
@@ -20,6 +20,46 @@ class TestItem < MiniTest::Test
   def test_item_has_name()
     result = @item.name()
     assert_equal("TV", result)
+  end
+
+  def test_item_has_id()
+    result = @item.id()
+    assert_equal(1, result)
+  end
+
+  def test_item_has_manufacturer_id()
+    result = @item.manufacturer_id()
+    assert_equal(20, result)
+  end
+
+  def test_item_has_description()
+    result = @item.description()
+    assert_equal("Smart TV", result)
+  end
+
+  def test_item_has_cost_price()
+    result = @item.cost_price()
+    assert_equal(100.11, result)
+  end
+
+  def test_item_has_sell_price()
+    result = @item.sell_price()
+    assert_equal(200, result)
+  end
+
+  def test_item_has_quantity_in_stock()
+    result = @item.quantity_in_stock()
+    assert_equal(10, result)
+  end
+
+  def test_item_has_min_stock_level()
+    result = @item.min_stock_level()
+    assert_equal(2, result)
+  end
+
+  def test_item_has_max_stock_level()
+    result = @item.max_stock_level()
+    assert_equal(50, result)
   end
 
 
