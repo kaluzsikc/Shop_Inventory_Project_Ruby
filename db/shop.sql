fa84db6ff61d4c0a76fe56cf44ef58aeab132991
kaluzsikc <kaluzsikc@gmail.com>
2018-08-24 14:53:46 +0100

    Removed many to many relationship

--- db/shop.sql
+++ db/shop.sql
@@ -1,13 +1,12 @@
-DROP TABLE manufacturers;
 DROP TABLE items;
+DROP TABLE manufacturers;
 
 
 CREATE TABLE manufacturers(
   id SERIAL4 PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   adress VARCHAR(255) NOT NULL,
-  telephone INT4 NOT NULL,
-  item_id REFERENCES items(id) ON DELETE CASCADE
+  telephone INT4 NOT NULL
 );
 
 CREATE TABLE items(
@@ -19,5 +18,5 @@ CREATE TABLE items(
   quantity_in_stock INT4 NOT NULL,
   min_stock_level INT4 NOT NULL,
   max_stock_level INT4 NOT NULL,
-  manufacturers_id INT4 REFERENCES manufacturers(id) ON DELETE CASCADE,
+  manufacturers_id INT4 REFERENCES manufacturers(id) ON DELETE CASCADE
 );
