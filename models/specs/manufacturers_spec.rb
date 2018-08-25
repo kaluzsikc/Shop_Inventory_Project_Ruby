3d3af877c44df39b6b46d2e69274db8a2ad908da
kaluzsikc <kaluzsikc@gmail.com>
2018-08-25 15:24:54 +0100

    Test manufacturer class had id, name,address and telephone

--- models/specs/manufacturers_spec.rb
+++ models/specs/manufacturers_spec.rb
@@ -1,2 +1,36 @@
 require("minitest/autorun")
 require_relative("../manufacturer")
+
+class TestManufacurer < MiniTest::Test
+
+  def setup
+    options = {"id" =>10,
+               "name" => "LG",
+               "address" => "123 Lothian Road, Edinburgh, UK",
+               "telephone" => "123456789"}
+    @manufacturer = Manufacturer.new(options)
+  end
+
+  def test_manufacurer_has_id()
+    result = @manufacturer.id()
+    assert_equal(10, result)
+  end
+
+  def test_manufacurer_has_name()
+    result = @manufacturer.name()
+    assert_equal("LG", result)
+  end
+
+  def test_manufacurer_has_address()
+    result = @manufacturer.address()
+    assert_equal("123 Lothian Road, Edinburgh, UK", result)
+  end
+
+  def test_manufacurer_has_telephone()
+    result = @manufacturer.telephone()
+    assert_equal("123456789", result)
+  end
+
+
+
+end
