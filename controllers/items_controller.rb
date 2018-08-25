4cfef07f90c5ff05e9d549762c3e56fd190c31e4
kaluzsikc <kaluzsikc@gmail.com>
2018-08-25 16:42:07 +0100

    Add items display page

--- controllers/items_controller.rb
+++ controllers/items_controller.rb
@@ -4,3 +4,8 @@ require( 'pry-byebug' )
 require_relative( '../models/item.rb' )
 require_relative( '../models/manufacturer.rb' )
 also_reload( '../models/*' )
+
+get '/items' do
+  @items = Item.all()
+  erb ( :'items/index')
+end
