8aac7421b94eb1a949eef5039e4ed97e16fafd6a
kaluzsikc <kaluzsikc@gmail.com>
2018-08-27 21:38:27 +0100

    Add manufacturer index and route

--- controllers/manufacturers_controller.rb
+++ controllers/manufacturers_controller.rb
@@ -7,3 +7,9 @@ get '/manufacturers' do
   @manufacturers = Manufacturer.all()
   erb ( :'manufacturers/index')
 end
+
+#Show
+get '/manufacturers/:id' do
+  @manufacturer = Manufacturer.find(params['id'].to_i)
+  erb ( :'manufacturers/show' )
+end
